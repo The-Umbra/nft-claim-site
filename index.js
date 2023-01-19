90c27558c84cde37d66a16dbeec2004a3dfaf2ca
UmbraPrime <yavuzsan13@gmail.com>
2023-01-19 20:13:35 +0300

    progress bar reworked

--- index.js
+++ index.js
@@ -13,3 +13,17 @@ const navMenu = document.querySelector(".nav-menu");
 menuIcon.addEventListener("click", () => {
   navMenu.classList.toggle("show");
 });
+
+// PROGRESS BAR ANIMATE
+
+$(".two-part-section .main-container .right-side .animated-progress span").each(
+  function () {
+    $(this).animate(
+      {
+        width: $(this).attr("data-progress") + "%",
+      },
+      1000
+    );
+    $(this).text($(this).attr("data-progress") + "%");
+  }
+);
